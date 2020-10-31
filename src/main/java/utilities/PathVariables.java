46474a3aee7f918f900ba9bfcd7973b6fdc8b0d5
MMAZ <mark-matthias.zymla@uni-konstanz.de>
2020-10-31 19:59:45 +0100

    fixed mvn -jar file production (requires user to install gswb with: mvn install:install-file -Dfile=resources\glueSemWorkbench2.jar -DgroupId=uni.kn.zymla -DartifactId=gswb -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true

--- src/main/java/utilities/PathVariables.java
+++ src/main/java/utilities/PathVariables.java
@@ -8,13 +8,16 @@ public class PathVariables {
     //Path to the dict package
     public static final String dictPath = "C:\\Users\\Celeste\\IdeaProjects\\SpringDemo\\resources\\dicts\\";
 
-/*
-    public static void main(String[] args) throws URISyntaxException {
-        System.out.println(PathVariables.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath().toString());
-        System.out.println(ClassLoader.getSystemClassLoader().getResource(".").getPath().toString());
 
+    public static void main(String[] args) {
+        try {
+            System.out.println(PathVariables.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath().toString());
+            System.out.println(ClassLoader.getSystemClassLoader().getResource(".").getPath().toString());
+        }
+        catch(Exception e)
+        {System.out.println("failed");}
     }
-*/
+
  //   return new File(MyClass.class.getProtectionDomain().getCodeSource().getLocation()
  //   .toURI()).getPath();
 
