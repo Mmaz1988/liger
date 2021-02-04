c90b2195d67eac9e9d62f58b59cf271d2d829ba5
Mmaz1988 <mark-matthias.zymla@uni-konstanz.de>
2021-02-04 10:28:57 +0100

    updated lfg testrules no 8.

--- src/main/java/utilities/PathVariables.java
+++ src/main/java/utilities/PathVariables.java
@@ -4,9 +4,9 @@ import java.net.URISyntaxException;
 
 public class PathVariables {
     //Path to the test package
-    public static final String testPath = "C:\\Users\\Celeste\\IdeaProjects\\SpringDemo\\resources\\testFiles\\";
+    public static final String testPath = "/Users/red_queen/IdeaProjects/syntax-annotator-glue/resources/testFiles/";
     //Path to the dict package
-    public static final String dictPath = "C:\\Users\\Celeste\\IdeaProjects\\SpringDemo\\resources\\dicts\\";
+    public static final String dictPath = "/Users/red_queen/IdeaProjects/syntax-annotator-glue/resources/dicts/";
 
 
     public static void main(String[] args) {
