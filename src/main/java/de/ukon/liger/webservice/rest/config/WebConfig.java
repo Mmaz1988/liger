b437c4861ddce992769c8765da790a8b261179fb
vollmer@dbvis.inf.uni-konstanz.de <vollmer@dbvis.inf.uni-konstanz.de>
2023-02-09 10:31:09 +0100

    [#14] simplified web config

--- src/main/java/de/ukon/liger/webservice/rest/config/WebConfig.java
+++ src/main/java/de/ukon/liger/webservice/rest/config/WebConfig.java
@@ -1,22 +1,17 @@
 package de.ukon.liger.webservice.rest.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer{
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                WebMvcConfigurer.super.addCorsMappings(registry);
-                registry.addMapping("/**").allowedOriginPatterns("*");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOriginPatterns("*");
     }
 
 }
