cbdeb36653286d4e68f8ca9fcc857200e8ef2711
vollmer@dbvis.inf.uni-konstanz.de <vollmer@dbvis.inf.uni-konstanz.de>
2023-02-09 11:05:32 +0100

    [#14] removed webconfig

--- src/main/java/de/ukon/liger/webservice/rest/config/WebConfig.java
+++ src/main/java/de/ukon/liger/webservice/rest/config/WebConfig.java
@@ -1,17 +1,17 @@
-package de.ukon.liger.webservice.rest.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer{
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOriginPatterns("*");
-    }
-
-}
+//package de.ukon.liger.webservice.rest.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//@EnableWebMvc
+//public class WebConfig implements WebMvcConfigurer{
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**").allowedOriginPatterns("*");
+//    }
+//
+//}
