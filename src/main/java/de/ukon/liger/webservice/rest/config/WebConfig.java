203940bf529fa516d1e1cb3a98c7bd7dce73c71c
vollmer@dbvis.inf.uni-konstanz.de <vollmer@dbvis.inf.uni-konstanz.de>
2023-02-09 10:07:35 +0100

    [#14] re-enabled web config

--- src/main/java/de/ukon/liger/webservice/rest/config/WebConfig.java
+++ src/main/java/de/ukon/liger/webservice/rest/config/WebConfig.java
@@ -1,22 +1,22 @@
-//package de.ukon.liger.webservice.rest.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig {
-//
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                WebMvcConfigurer.super.addCorsMappings(registry);
-//                registry.addMapping("/**").allowedOriginPatterns("*");
-//            }
-//        };
-//    }
-//
-//}
+package de.ukon.liger.webservice.rest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                WebMvcConfigurer.super.addCorsMappings(registry);
+                registry.addMapping("/**").allowedOriginPatterns("*");
+            }
+        };
+    }
+
+}
