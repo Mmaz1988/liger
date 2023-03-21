0d8aca667a7b1ab6851869078df9448fc6cb9cc1
mmaz1988 <mark-matthias.zymla@uni-konstanz.de>
2023-03-21 22:44:19 +0100

    removed queryparser2

--- src/main/java/de/ukon/liger/analysis/QueryParser/QueryParser2.java
+++ src/main/java/de/ukon/liger/analysis/QueryParser/QueryParser2.java
@@ -28,44 +28,11 @@ import java.util.LinkedList;
 
 public class QueryParser2 {
 
-    private int pos = 0;
-
-    // LinkedList<QueryExpression> parsedExpressions = new LinkedList<>();
-
-    public QueryExpression parseQuery(String query, LinkedList<QueryExpression> parsedExpressions, HashMap<Integer, GraphConstraint> fsIndices)
+    public QueryParser2()
     {
-        while (query.charAt(pos) == ' ')
-        {
-            pos++;
-        }
 
-        char c = query.charAt(pos);
-
-
-        if (c == '#')
-        {
-            pos++;
-
-            StringBuilder sb = new StringBuilder();
-            while(query.charAt(pos) != ' ')
-            {
-                sb.append(query.charAt(pos));
-                pos++;
-            }
-            String varName = sb.toString();
-
-            Node n = new Node(varName,fsIndices);
-
-
-
-
-        }
-
-        if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-        {
-
-        }
-
-        return null;
     }
+
+
+
 }
