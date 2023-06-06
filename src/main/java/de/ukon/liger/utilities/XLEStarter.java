5c0df3548204bcafd37324f21c13e1d64c19eed6
mmaz1988 <mark-matthias.zymla@uni-konstanz.de>
2023-06-06 13:19:48 +0200

    automated XLE parsing for Windows. Needs to be tested on mac next.

---
+++ src/main/java/de/ukon/liger/utilities/XLEStarter.java
@@ -0,0 +1,119 @@
+package de.ukon.liger.utilities;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+public class XLEStarter {
+
+    public enum OS {
+        WINDOWS, LINUX, MAC, SOLARIS
+    }
+
+    ;
+
+    public String xlePath;
+    public String grammarPath;
+    public final OS operatingSystem;
+
+    public XLEStarter(String xlePath, String grammarPath, OS operatingSystem) {
+        this.xlePath = xlePath;
+        this.grammarPath = grammarPath;
+        this.operatingSystem = operatingSystem;
+    }
+
+
+    public void generateXLEStarterFile() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("#!/bin/bash\n");
+
+        /*
+        export XLEPATH=/bin/xle
+        export PATH=${XLEPATH}/bin:$PATH
+        export DYLD_LIBRARY_PATH=$XLEPATH/lib:$XLEPATH/bin/sp-3.12.7
+        export LD_LIBRARY_PATH=${XLEPATH}/lib
+        export LD_LIBRARY_PATH=${XLEPATH}/lib:$LD_LIBRARY_PATH
+        export DYLD_LIBRARY_PATH=${XLEPATH}/lib:$DYLD_LIBRARY_PATH
+         */
+
+        sb.append("export XLEPATH=");
+        sb.append(xlePath);
+        sb.append("\n");
+        sb.append("export PATH=${XLEPATH}/bin:$PATH");
+        sb.append("\n");
+        sb.append("export DYLD_LIBRARY_PATH=$XLEPATH/lib:$XLEPATH/bin/sp-3.12.7");
+        sb.append("\n");
+        sb.append("export LD_LIBRARY_PATH=${XLEPATH}/lib");
+        sb.append("\n");
+        sb.append("export LD_LIBRARY_PATH=${XLEPATH}/lib:$LD_LIBRARY_PATH");
+        sb.append("\n");
+        sb.append("export DYLD_LIBRARY_PATH=${XLEPATH}/lib:$DYLD_LIBRARY_PATH");
+        sb.append("\n");
+        sb.append("\n");
+
+        if (this.operatingSystem.equals(OS.WINDOWS)) {
+            /*
+            export TCL_LIBRARY=${XLEPATH}/tcl/scripts/tcl
+            export TCLLIBPATH=${XLEPATH}/tcl/scripts/tcl
+            export TKLIBPATH=${XLEPATH}/tcl/scripts/tk
+            export TK_LIBRARY=${XLEPATH}/tcl/scripts/tk
+             */
+            sb.append("export TCL_LIBRARY=${XLEPATH}/tcl/scripts/tcl");
+            sb.append("\n");
+            sb.append("export TCLLIBPATH=${XLEPATH}/tcl/scripts/tcl");
+            sb.append("\n");
+            sb.append("export TKLIBPATH=${XLEPATH}/tcl/scripts/tk");
+            sb.append("\n");
+            sb.append("export TK_LIBRARY=${XLEPATH}/tcl/scripts/tk");
+            sb.append("\n");
+            sb.append("\n");
+        }
+
+        // xle -noTk -e "create-parser /mnt/d/Resources/english_pargram/index/main.lfg; parse-testfile testfile.lfg -outputPrefix parser_output/sentence; exit"
+
+        sb.append("xle -noTk -e \"create-parser ");
+        sb.append(grammarPath);
+        sb.append("; parse-testfile ");
+
+
+        String testFileString = Paths.get(PathVariables.workingDirectory, "tmp","testfile.lfg").toString();
+
+        if (operatingSystem.equals(OS.WINDOWS)){
+            testFileString = HelperMethods.formatWslString(testFileString);
+        }
+
+        sb.append(testFileString);
+
+        String outputPrefix = Paths.get(PathVariables.workingDirectory, "tmp","parser_output/sentence").toString();
+
+        if (operatingSystem.equals(OS.WINDOWS)){
+            outputPrefix = HelperMethods.formatWslString(outputPrefix);
+        }
+
+
+        sb.append(" -outputPrefix ");
+        sb.append(outputPrefix);
+        sb.append("; exit\"");
+        sb.append("\n");
+
+
+        //open file and write
+        File file = new File(Paths.get(PathVariables.workingDirectory, "tmp", "xlebash.sh").toString());
+
+        if (file.exists())
+        {
+            file.delete();
+        }
+
+        try {
+            java.io.FileWriter fw = new java.io.FileWriter(file);
+            fw.write(sb.toString());
+            fw.close();
+        } catch (
+                Exception e) {
+            System.out.println("Failed to write xlebash.sh");
+        }
+    }
+    }
+
+
+
